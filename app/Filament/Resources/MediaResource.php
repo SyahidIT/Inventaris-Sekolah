a61033b06af69554f5c15e6a679ca23d0263ad2d
Muhammad Syahid Abdillah <muhammad.syahid39@gmail.com>
2023-08-17 22:37:31 +0700

    add media

---
+++ app/Filament/Resources/MediaResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MediaResource\Pages;
+use App\Filament\Resources\MediaResource\RelationManagers;
+use App\Models\Media;
+use Filament\Forms;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\SpatieMediaLibraryImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MediaResource extends Resource
+{
+    protected static ?string $model = Media::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-photo';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->label('Nama'),
+                SpatieMediaLibraryFileUpload::make('attachments')
+                    ->multiple()
+                    ->enableReordering(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                SpatieMediaLibraryImageColumn::make('name')->label('Nama'),
+                SpatieMediaLibraryImageColumn::make('attachments')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMedia::route('/'),
+            'create' => Pages\CreateMedia::route('/create'),
+        ];
+    }    
+}
---
+++ app/Models/Media.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+    use HasFactory;
+    protected $guarded = [];
+}
